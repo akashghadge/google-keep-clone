0fc637c297bffc7662f841ae0ff2280e1ed2d86c
akash_ghadge <asghadge6@gmail.com>
2021-01-15 11:33:10 +0530

    google keep clone version 1.0

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Notes from "./components/Notes"
+import Create from "./components/Create"
+function App() {
+  return (
+    <>
+      <Header />
+      <Create />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
